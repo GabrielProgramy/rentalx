4a741e965f3096a8a3cc59261cf8a5d3b2b10786
Gabriel Costa <gabrielestevan14@gmail.com>
2021-05-26 16:49:49 -0300

    exception handling

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
 
@@ -13,7 +14,7 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error("Token missing");
+    throw new AppError("Token missing", 401);
   }
 
   const [, token] = authHeader.split(" ");
@@ -24,11 +25,11 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
     const user = usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error("User does not exists!");
+      throw new AppError("User does not exists!", 401);
     }
 
     next();
   } catch (error) {
-    throw new Error("Invalid Token!");
+    throw new AppError("Invalid Token!", 401);
   }
 }
